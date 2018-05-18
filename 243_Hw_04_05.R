73c3754eb01c171d19c01248dbd749131196a8b5
smaillie16 <34048406+smaillie16@users.noreply.github.com>
2018-05-18 04:40:34 -0700

    Add files via upload

--- 243_Hw_04_05.R
+++ 243_Hw_04_05.R
@@ -1,2 +1,17 @@
-#Samantha Maillie
+#Samantha Maillie and Ariel Sim
 #Homework 4 Problem 5
+
+
+#a
+n <- 100
+p <- .3
+lambda <- 2
+
+y <- rpois(n, lambda)
+r <- rbinom(n, 1, p)
+x = y*r
+hist(x, main = "Histogram of X", col = "coral")
+
+#b
+
+#c
