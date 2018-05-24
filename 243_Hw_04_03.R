573428cb24fbf41f10c2181caf356aa67cf4522b
A.P Sim <apsim@ucdavis.edu>
2018-05-23 17:07:34 -0700

    updated with b

--- 243_Hw_04_03.R
+++ 243_Hw_04_03.R
@@ -31,3 +31,28 @@ for (i in 1:n){
 }
 i = total/n
 i
+
+# a
+n = 1500
+U = runif(n,0,1)
+h = 1/(1+U)
+I_mc = sum(h)/n
+I_mc
+log(2) # comparable
+
+# b
+U2 = runif(n,0,1)
+c = 1 + U2
+theta_mc = sum(c)/n
+theta_mc
+
+# Calculating the optimal b:
+num = sum((c - theta_mc)*(h - I_mc))
+denom = sum((c - theta_mc)^2)
+b = num/denom
+b
+
+# Therefore
+I_cv = I_mc - b*(theta_mc-0.5)
+I_cv
+
